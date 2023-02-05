fc35f9d408b98172e6438930109baaca6cdfbf08
Chibueze-Adeyemi-Ajayi <chibuezeadeyemi@gmail.com>
2023-02-05 20:45:34 +0100

    My Portfolio

--- src/components/skill.js
+++ src/components/skill.js
@@ -1,7 +1,11 @@
 const Card = (props) => {
-    <div className="w-full h-fit p-3 flex flex-col space-y-2 bg-black">
-        Hola
-    </div>
+    return  (
+        <div className="w-full h-fit p-3 flex flex-col space-y-4 bg-white/30">
+            {props.icon}
+            <h1 className="text-center font-bold text-xl cursor-pointer">{props.title}</h1>
+            <font className="text-center">{props.content}</font>
+        </div>
+    );
 }
 
 const Skill = () => {
@@ -9,8 +13,28 @@ const Skill = () => {
         <div className="p-6">
             <div className="bg-inherit border border-white/30 rounded-md p-3 flex flex-col space-y-4">
                 <h1 className="text-white/90 text-3xl font-extrabold border-b border-white/30 pb-3">Skills & Services</h1>
-                <section className="w-full h-full p-6 grid grid-cols-3 gap-6">
-                    <Card />
+                <section className="w-full h-full p-6 grid grid-cols-4 gap-6">
+                    <Card icon={
+                        <svg className="w-24 mx-auto h-24" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 640 512">
+                            <path className="fill-black" d="M255.03 261.65c6.25 6.25 16.38 6.25 22.63 0l11.31-11.31c6.25-6.25 6.25-16.38 0-22.63L253.25 192l35.71-35.72c6.25-6.25 6.25-16.38 0-22.63l-11.31-11.31c-6.25-6.25-16.38-6.25-22.63 0l-58.34 58.34c-6.25 6.25-6.25 16.38 0 22.63l58.35 58.34zm96.01-11.3l11.31 11.31c6.25 6.25 16.38 6.25 22.63 0l58.34-58.34c6.25-6.25 6.25-16.38 0-22.63l-58.34-58.34c-6.25-6.25-16.38-6.25-22.63 0l-11.31 11.31c-6.25 6.25-6.25 16.38 0 22.63L386.75 192l-35.71 35.72c-6.25 6.25-6.25 16.38 0 22.63zM624 416H381.54c-.74 19.81-14.71 32-32.74 32H288c-18.69 0-33.02-17.47-32.77-32H16c-8.8 0-16 7.2-16 16v16c0 35.2 28.8 64 64 64h512c35.2 0 64-28.8 64-64v-16c0-8.8-7.2-16-16-16zM576 48c0-26.4-21.6-48-48-48H112C85.6 0 64 21.6 64 48v336h512V48zm-64 272H128V64h384v256z"/>
+                        </svg>
+                    } title="Frontend Developement" content="By leveraging my expertise on tools like HTML, CSS, Javascript and their respective frameworks, I can build sophisticated Graphic User Interfaces for your web applications"/>
+                    <Card icon={
+                        <svg className="w-24 mx-auto h-24" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512">
+                            <path className="fill-black" d="M448 73.143v45.714C448 159.143 347.667 192 224 192S0 159.143 0 118.857V73.143C0 32.857 100.333 0 224 0s224 32.857 224 73.143zM448 176v102.857C448 319.143 347.667 352 224 352S0 319.143 0 278.857V176c48.125 33.143 136.208 48.572 224 48.572S399.874 209.143 448 176zm0 160v102.857C448 479.143 347.667 512 224 512S0 479.143 0 438.857V336c48.125 33.143 136.208 48.572 224 48.572S399.874 369.143 448 336z"/>
+                        </svg>
+                    } title="Backend Developement" content="Languages like Php, Java, Python, Node.JS and their respective frameworks are tools I use to build backend endpoint for both your web and mobile applications."/>
+                    <Card icon={
+                        <svg className="w-24 mx-auto h-24" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 640 512">
+                            <path className="fill-black" d="M255.03 261.65c6.25 6.25 16.38 6.25 22.63 0l11.31-11.31c6.25-6.25 6.25-16.38 0-22.63L253.25 192l35.71-35.72c6.25-6.25 6.25-16.38 0-22.63l-11.31-11.31c-6.25-6.25-16.38-6.25-22.63 0l-58.34 58.34c-6.25 6.25-6.25 16.38 0 22.63l58.35 58.34zm96.01-11.3l11.31 11.31c6.25 6.25 16.38 6.25 22.63 0l58.34-58.34c6.25-6.25 6.25-16.38 0-22.63l-58.34-58.34c-6.25-6.25-16.38-6.25-22.63 0l-11.31 11.31c-6.25 6.25-6.25 16.38 0 22.63L386.75 192l-35.71 35.72c-6.25 6.25-6.25 16.38 0 22.63zM624 416H381.54c-.74 19.81-14.71 32-32.74 32H288c-18.69 0-33.02-17.47-32.77-32H16c-8.8 0-16 7.2-16 16v16c0 35.2 28.8 64 64 64h512c35.2 0 64-28.8 64-64v-16c0-8.8-7.2-16-16-16zM576 48c0-26.4-21.6-48-48-48H112C85.6 0 64 21.6 64 48v336h512V48zm-64 272H128V64h384v256z"/>
+                        </svg>
+                    } title="Frontend Developement" content="By leveraging my expertise on tools like HTML, CSS, Javascript and their respective framewworks, I can build sophisticated Graphic User Interfaces for your web applications"/>
+                    <Card icon={
+                        <svg className="w-24 mx-auto h-24" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 640 512">
+                            <path className="fill-black" d="M255.03 261.65c6.25 6.25 16.38 6.25 22.63 0l11.31-11.31c6.25-6.25 6.25-16.38 0-22.63L253.25 192l35.71-35.72c6.25-6.25 6.25-16.38 0-22.63l-11.31-11.31c-6.25-6.25-16.38-6.25-22.63 0l-58.34 58.34c-6.25 6.25-6.25 16.38 0 22.63l58.35 58.34zm96.01-11.3l11.31 11.31c6.25 6.25 16.38 6.25 22.63 0l58.34-58.34c6.25-6.25 6.25-16.38 0-22.63l-58.34-58.34c-6.25-6.25-16.38-6.25-22.63 0l-11.31 11.31c-6.25 6.25-6.25 16.38 0 22.63L386.75 192l-35.71 35.72c-6.25 6.25-6.25 16.38 0 22.63zM624 416H381.54c-.74 19.81-14.71 32-32.74 32H288c-18.69 0-33.02-17.47-32.77-32H16c-8.8 0-16 7.2-16 16v16c0 35.2 28.8 64 64 64h512c35.2 0 64-28.8 64-64v-16c0-8.8-7.2-16-16-16zM576 48c0-26.4-21.6-48-48-48H112C85.6 0 64 21.6 64 48v336h512V48zm-64 272H128V64h384v256z"/>
+                        </svg>
+                    } title="Frontend Developement" content="By leveraging my expertise on tools like HTML, CSS, Javascript and their respective framewworks, I can build sophisticated Graphic User Interfaces for your web applications"/>
+
                 </section>
             </div>
         </div>
