ff1c9ebecfe92e9aff7194c13788f00b4b08ad75
Chibueze-Adeyemi-Ajayi <chibuezeadeyemi@gmail.com>
2023-02-06 00:33:38 +0100

    My portfolio

--- src/components/contact.js
+++ src/components/contact.js
@@ -1,8 +1,11 @@
 const Contact = () => {
     return (
-        <section>
-            
-        </section>
+        <div className="w-full h-full p-6">
+            <section className="w-full h-full flex flex-col space-y-4">
+                <h1 className="text-2xl font-extrabold text-white/90 py-3 pl-6 border-y border-white/30">Let's get in touch</h1>
+            </section>
+        </div>
+        
     );
 }
 
