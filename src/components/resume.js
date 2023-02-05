dfa327d5f416da5ab6eefb36c34d46d594f93261
Chibueze-Adeyemi-Ajayi <chibuezeadeyemi@gmail.com>
2023-02-05 18:55:46 +0100

    My portfolio

--- src/components/resume.js
+++ src/components/resume.js
@@ -1,6 +1,11 @@
 const Resume = () => {
     return (
+        <section className="w-full h-full flex flex-row space-x-4 p-12">
+            
+            <div className="w-full h-fit flex flex-col space-y-4 p-6 rounded-xl border border-white/30">
 
+            </div>
+        </section>
     );
 }
 
