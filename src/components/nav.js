eee7b0953c563c5c7219bd69162bc6ff86e9b913
Chibueze-Adeyemi-Ajayi <chibuezeadeyemi@gmail.com>
2023-02-05 17:55:07 +0100

    My portfolio

--- src/components/nav.js
+++ src/components/nav.js
@@ -1,7 +1,12 @@
 const Nav = () => {
     return (
-        <nav className="w-full h-[40px] border-b border-[#444] px-10">
-            
+        <nav className="w-full h-[40px] grid grid-cols-3 border-b border-[#444] px-10">
+            <font className="text-white/90 font-bold my-auto">Ajayi Chibueze Adeyemi</font>
+            <div className="w-full col-span-2 flex">
+                <div className="w-fit h-fit mx-auto my-auto flex space-x-3">
+
+                </div>
+            </div>
         </nav>
     );
 }
