5214fc01f26087dedd44936fb8ac7c78d62d8229
Chibueze-Adeyemi-Ajayi <chibuezeadeyemi@gmail.com>
2023-02-05 20:28:45 +0100

    My Portfolio

--- src/components/skill.js
+++ src/components/skill.js
@@ -7,8 +7,8 @@ const Card = (props) => {
 const Skill = () => {
     return (
         <div className="p-6">
-            <div className="bg-white/30 p-3 flex flex-col space-y-4">
-                <h1 className="text-black text-3xl font-extrabold border-b-4 border-black pb-2">Skills & Services</h1>
+            <div className="bg-inherit border border-white/30 rounded-md p-3 flex flex-col space-y-4">
+                <h1 className="text-white/90 text-3xl font-extrabold border-b border-white/30 pb-3">Skills & Services</h1>
                 <section className="w-full h-full p-6 grid grid-cols-3 gap-6">
                     <Card />
                 </section>
