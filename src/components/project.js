0a384f50eaeeff317ce17f40e41b9de87783afba
Chibueze-Adeyemi-Ajayi <chibuezeadeyemi@gmail.com>
2023-02-05 23:25:35 +0100

    My portfolio

--- src/components/project.js
+++ src/components/project.js
@@ -16,7 +16,7 @@ const Card = (props) => {
                     <svg className="w-4 h-4 my-auto" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512">
                         <path className="fill-white/90" d="M326.612 185.391c59.747 59.809 58.927 155.698.36 214.59-.11.12-.24.25-.36.37l-67.2 67.2c-59.27 59.27-155.699 59.262-214.96 0-59.27-59.26-59.27-155.7 0-214.96l37.106-37.106c9.84-9.84 26.786-3.3 27.294 10.606.648 17.722 3.826 35.527 9.69 52.721 1.986 5.822.567 12.262-3.783 16.612l-13.087 13.087c-28.026 28.026-28.905 73.66-1.155 101.96 28.024 28.579 74.086 28.749 102.325.51l67.2-67.19c28.191-28.191 28.073-73.757 0-101.83-3.701-3.694-7.429-6.564-10.341-8.569a16.037 16.037 0 0 1-6.947-12.606c-.396-10.567 3.348-21.456 11.698-29.806l21.054-21.055c5.521-5.521 14.182-6.199 20.584-1.731a152.482 152.482 0 0 1 20.522 17.197zM467.547 44.449c-59.261-59.262-155.69-59.27-214.96 0l-67.2 67.2c-.12.12-.25.25-.36.37-58.566 58.892-59.387 154.781.36 214.59a152.454 152.454 0 0 0 20.521 17.196c6.402 4.468 15.064 3.789 20.584-1.731l21.054-21.055c8.35-8.35 12.094-19.239 11.698-29.806a16.037 16.037 0 0 0-6.947-12.606c-2.912-2.005-6.64-4.875-10.341-8.569-28.073-28.073-28.191-73.639 0-101.83l67.2-67.19c28.239-28.239 74.3-28.069 102.325.51 27.75 28.3 26.872 73.934-1.155 101.96l-13.087 13.087c-4.35 4.35-5.769 10.79-3.783 16.612 5.864 17.194 9.042 34.999 9.69 52.721.509 13.906 17.454 20.446 27.294 10.606l37.106-37.106c59.271-59.259 59.271-155.699.001-214.959z"/>
                     </svg>
-                    <font className="text-white/90">Source Code</font>
+                    <font className="text-white/90">View App</font>
                 </a> : <></> }
             </div>
         </div>
@@ -25,7 +25,7 @@ const Card = (props) => {
 
 const Projects = () => {
     return (
-        <scetion className="w-full h-full p-6 flex">
+        <scetion className="w-full h-full p-6 flex space-x-4">
             <section className="lg:w-[500px] md:w-[400px] h-fit my-auto flex flex-col space-y-8">
                 <div className="w-full h-fit my-auto flex flex-col space-y-5">
                     <svg className="w-64 h-64 mx-auto my-auto" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 480 512">
@@ -39,9 +39,9 @@ const Projects = () => {
             <section className="w-full h-full border-l border-white/30">
                 <h1 className="text-white/90 font-bold px-4 py-2 border-y border-white/30 text-2xl">Live Projects</h1>
                 <div className="w-full h-full p-6 grid grid-cols-2 gap-6">
-                    <Card active={true} link="https://workspace.t21services.co.uk" source_code="https://github.com/Chibueze-Adeyemi-Ajayi/ReactChatSystem" language="JavaScript" color="bg-yellow-300" title="ReactChatSystem" content="This chat application serves the purpose of a company's workspace. Entire frontend built on REACT JS and the backend API on Laravel PHP" />
-                    <Card active={false} source_code="https://github.com/Crediometer/credio_ajo" language="Dart" color="bg-cyan-300" title="Credio Ajo App" content="Fully fledge contribution (ajo) app was worked upon with my team during the days of my internship with crediometer. Flutter was the framework used to achieve this." />
-                    <Card active={true} link="https://language-processor.netlify.app/" source_code="https://github.com/Chibueze-Adeyemi-Ajayi/LanguageProcessor" language="Dart" color="bg-cyan-300" title="Language Processor" content="This chat application serves the purpose of a company's workspace. Entire frontend built on REACT JS and the backend API on Laravel PHP" />
+                    <Card active={true} link="https://workspace.t21services.co.uk" source_code="https://github.com/Chibueze-Adeyemi-Ajayi/ReactChatSystem" language="JavaScript" color="bg-yellow-300" title="ReactChatSystem" content="This web application serves the purpose of a company's workspace. Entire frontend built on REACT JS and the backend API on Laravel PHP" />
+                    <Card active={false} source_code="https://github.com/Crediometer/credio_ajo" language="Dart" color="bg-cyan-300" title="Credio Ajo App" content="Fully fledge contribution (ajo) mobile/desktop app was worked upon with my team during the days of my internship with crediometer. Flutter was the framework used to achieve this." />
+                    <Card active={true} link="https://language-processor.netlify.app/" source_code="https://github.com/Chibueze-Adeyemi-Ajayi/LanguageProcessor" language="Dart" color="bg-cyan-300" title="Language Processor" content="A machine learning mobile application, which makes use of Natural Language Processing to understand human language then translate it to any choosen language" />
                     
                 </div>
             </section>
