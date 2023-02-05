bec9bf40e75604c3e3bfa0ab5ae4229656fe508f
Chibueze-Adeyemi-Ajayi <chibuezeadeyemi@gmail.com>
2023-02-05 18:17:33 +0100

    My portfolio

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import './App.css';
 import Nav from './components/nav';
+import Overview from './components/overview';
 
 function App() {
   return (
     <div className="w-full h-full bg-inherit">
       {/* nav bar */}
       <Nav/>
+      {/* overview */}
+      <Overview />
     </div>
   );
 }
--- src/components/overview.js
+++ src/components/overview.js
@@ -1,7 +1,12 @@
+import jilo_dev from "../assets/img/jilo_dev.jpg";
+
 const Overview = () => {
     return (
         <section className="w-full h-full flex flex-row space-x-4 p-6 mt-[60px]">
             {/* left tab */}
+            <section className="w-full h-full flex flex-col space-y-4">
+                <img src={jilo_dev}></img>
+            </section>
             {/* right tab */}
         </section>
     )
