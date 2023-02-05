ac2f8625633b7a97243499e8f0339da5cc0f3545
Chibueze-Adeyemi-Ajayi <chibuezeadeyemi@gmail.com>
2023-02-05 18:13:56 +0100

    My portfolio

--- src/components/overview.js
+++ src/components/overview.js
@@ -1,7 +1,8 @@
 const Overview = () => {
     return (
-        <section>
-
+        <section className="w-full h-full flex flex-row space-x-4 p-6 mt-[60px]">
+            {/* left tab */}
+            {/* right tab */}
         </section>
     )
 }
