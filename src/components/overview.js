b518db289dbb4ccd5168c9ef8561fb9b4c08b318
Chibueze-Adeyemi-Ajayi <chibuezeadeyemi@gmail.com>
2023-02-05 18:21:44 +0100

    My portfolio

--- src/components/overview.js
+++ src/components/overview.js
@@ -2,10 +2,12 @@ import jilo_dev from "../assets/img/jilo_dev.jpg";
 
 const Overview = () => {
     return (
-        <section className="w-full h-full flex flex-row space-x-4 p-6 mt-[60px]">
+        <section className="w-full h-full flex flex-row space-x-4 p-12 mt-[60px]">
             {/* left tab */}
-            <section className="w-full h-full flex flex-col space-y-4">
-                <img src={jilo_dev}></img>
+            <section className="lg:w-[400px] md:w-[300px] h-full flex flex-col space-y-4">
+                <img className="w-56 h-56 mx-auto rounded-full" src={jilo_dev}></img>
+                <font className="text-2xl mx-auto text-white/90 font-bold">Jilo Developer</font>
+                <font className="mx-auto text-white/90 text-xl">Ajayi Chibueze Adeyemi</font>
             </section>
             {/* right tab */}
         </section>
