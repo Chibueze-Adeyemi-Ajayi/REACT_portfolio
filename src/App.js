d4adec86810a9391b247c4ad6d5d15e0e7885d5c
Chibueze-Adeyemi-Ajayi <chibuezeadeyemi@gmail.com>
2023-02-19 16:43:48 +0100

    portfolio

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ import Resume from './components/resume';
 import SideBar from './components/side-bar';
 import Skill from './components/skill';
 
-// my port
+// my portf
 
 function App() {
   return (
