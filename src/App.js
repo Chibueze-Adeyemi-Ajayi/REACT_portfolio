e6f53b0df27c03a680062502cd8f2be9b7ccbe2a
Chibueze-Adeyemi-Ajayi <chibuezeadeyemi@gmail.com>
2023-02-19 16:58:41 +0100

    portfolio

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ import Resume from './components/resume';
 import SideBar from './components/side-bar';
 import Skill from './components/skill';
 
-// my portfolio application built REACT JavaScript fram
+// my portfolio application built REACT JavaScript frame
 
 function App() {
   return (
