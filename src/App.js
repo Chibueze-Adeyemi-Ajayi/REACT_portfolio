3135a8c216ebafc076c60998e365840bf7dbcdb0
Chibueze-Adeyemi-Ajayi <chibuezeadeyemi@gmail.com>
2023-02-19 16:46:44 +0100

    portfolio

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ import Resume from './components/resume';
 import SideBar from './components/side-bar';
 import Skill from './components/skill';
 
-// my portfolio appl
+// my portfolio appli
 
 function App() {
   return (
