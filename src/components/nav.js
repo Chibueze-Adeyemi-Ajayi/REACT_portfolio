02b8478a4368e34a3ea9fccec75f2e9b0ff11659
Chibueze-Adeyemi-Ajayi <chibuezeadeyemi@gmail.com>
2023-03-12 17:10:00 +0100

    portfolio

--- src/components/nav.js
+++ src/components/nav.js
@@ -4,7 +4,7 @@ import menu from "../assets/img/menu.png"
 import { getPos, setPos } from "../assets/js/nav";
 
 var nav_pos = -300;
-// Appli
+// Applic
 const Nav = () => {
     useEffect(() => {
         nav_pos = -300
