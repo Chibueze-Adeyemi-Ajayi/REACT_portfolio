7ea8136d96a721d2e0929a1a02ddcf78411c72c0
Chibueze-Adeyemi-Ajayi <chibuezeadeyemi@gmail.com>
2023-03-13 22:11:21 +0100

    portfolio

--- src/components/side-bar.js
+++ src/components/side-bar.js
@@ -51,7 +51,7 @@ const SideBar = () => {
                         <font className="text-white/90 text-sm">Overview</font>
                     </span>
                     <span onClick={
-                        // cl
+                        // clo
                         () => {
                             $("#side_bar").animate({left: "-300px"});
                             setPos(-300);
