e67d4a57515683c68d59c337986df8f4e9c08d94
Chibueze-Adeyemi-Ajayi <chibuezeadeyemi@gmail.com>
2023-04-18 23:18:45 +0100

    Portfolio

--- src/components/overview.js
+++ src/components/overview.js
@@ -1,6 +1,6 @@
 // my overview page displaying my basic information
 
-//  importing my image
+//  importing my imageb
 import jilo_dev from "../assets/img/jilo_dev.jpg";
 
 const Overview = () => {
