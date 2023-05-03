6c3bc683ec2aaccf96ee4ec7381cc6c6d6b0f9ca
Jilo developer <70321034+Chibueze-Adeyemi-Ajayi@users.noreply.github.com>
2023-05-03 05:13:47 +0100

    Create object detection.js

---
+++ object detection.js
@@ -0,0 +1,33 @@
+const express = require('express');
+const mysql = require('mysql');
+
+// Set up the database connection
+const connection = mysql.createConnection({
+  host: ' localhost:3306',
+  user: 'radiobox_radiobox',
+  password: 'zionfamily1',
+  database: 'radiobox_object detection'
+});
+
+// Create an Express.js app
+const app = express();
+
+// Set up a POST endpoint to receive detected content and store it in the database
+app.post('/detected-content', (req, res) => {
+  const { content } = req.body; // Assuming content is sent in the request body
+  const sql = 'INSERT INTO detected_content (content) VALUES (?)';
+  connection.query(sql, [content], (error, results, fields) => {
+    if (error) {
+      console.error(error);
+      res.status(500).send('Error storing content in database');
+    } else {
+      console.log('Content stored in database');
+      res.status(200).send('Content stored in database');
+    }
+  });
+});
+
+// Start the server
+app.listen(3000, () => {
+  console.log('Server listening on port 3000');
+});
